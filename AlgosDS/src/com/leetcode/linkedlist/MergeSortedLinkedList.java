68baacfec6ba101c1f9312730f2b2a6a940062c0
JToaddler <javasmoker-github@no-reply.com>
2023-05-22 22:13:54 -0400

    LeetCode Merge 2 Sorted LinkedList

--- AlgosDS/src/com/leetcode/linkedlist/MergeSortedLinkedList.java
+++ AlgosDS/src/com/leetcode/linkedlist/MergeSortedLinkedList.java
@@ -8,8 +8,55 @@ package com.leetcode.linkedlist;
  */
 public class MergeSortedLinkedList {
 
+	public ListNode mergeLinkedList(ListNode list1, ListNode list2) {
+
+		if (list1 == null)
+			return list2;
+		if (list2 == null)
+			return list1;
+		ListNode node = new ListNode(0);
+		ListNode next = node;
+
+		while (list1 != null && list2 != null) {
+
+			if (list1.val > list2.val) {
+				next.next = new ListNode(list2.val);
+				list2 = list2.next;
+			} else {
+				next.next = new ListNode(list1.val);
+				list1 = list1.next;
+			}
+			next = next.next;
+		}
+		next.next = list1 != null ? list1 : list2;
+		return node.next;
+	}
+
+	public ListNode compareInsert(ListNode list1, ListNode list2, ListNode node) {
+
+		if (list1.val < list2.val) {
+			node.val = list1.val;
+			node.next = new ListNode(list2.val);
+		} else {
+			node.val = list2.val;
+			node.next = new ListNode(list1.val);
+		}
+		return node.next;
+	}
+
 	public static void main(String[] args) {
-		
+		ListNode node1 = new ListNode(5);
+		node1.insert(12);
+		node1.insert(34);
+		node1.insert(56);
+
+		ListNode node2 = new ListNode(3);
+		node2.insert(11);
+		node2.insert(39);
+		node2.insert(46);
+
+		ListNode mergedList = new MergeSortedLinkedList().mergeLinkedList(node1, node2);
+		System.out.println(mergedList);
 	}
 
 }
@@ -31,20 +78,35 @@ class ListNode {
 		this.next = next;
 	}
 
-	public void insert(int val) {
+	@Override
+	public String toString() {
 
+		StringBuilder sb = new StringBuilder();
+		sb.append("val :" + this.val + ", ").append("[ ").append(this.val);
 		if (next == null) {
-			next = new ListNode(val);
+			sb.append(" ]");
+		} else {
+			ListNode current = this.next;
+			while (current.next != null) {
+				sb.append(", ").append(current.val);
+				current = current.next;
+			}
+			sb.append(", " + current.val).append(" ]");
+		}
+		return sb.toString();
+	}
+
+	public void insert(int val) {
+		ListNode node = new ListNode(val);
+		if (this.next == null) {
+			this.next = node;
 		} else {
 			ListNode current = next;
-			while (current != null) {
-
+			while (current.next != null) {
 				current = current.next;
-
 			}
-
+			current.next = node;
 		}
-
 	}
 
 }
