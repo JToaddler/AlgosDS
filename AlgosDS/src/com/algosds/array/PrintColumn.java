129c606908f5fee159332d9b1d12e014f7b85386
JToaddler <JToaddler@users.noreply.github.com>
2023-05-26 15:45:50 -0400

    2D Array updated

--- AlgosDS/src/com/algosds/array/PrintColumn.java
+++ AlgosDS/src/com/algosds/array/PrintColumn.java
@@ -1,5 +1,34 @@
 package com.algosds.array;
 
+import java.util.Arrays;
+
 public class PrintColumn {
+
+	public static void main(String[] args) {
+
+		Integer[][] nums = generateGrid(4, 4);
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < nums[0].length; i++) {
+			for (int j = 0; j < nums.length; j++) {
+				sb.append(nums[j][i] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+
+	public static Integer[][] generateGrid(int rCount, int cCount) {
+		int num = 0;
+		Integer[][] randomGrid = new Integer[rCount][cCount];
+		for (int i = 0; i < rCount; i++) {
+			for (int j = 0; j < cCount; j++) {
+				randomGrid[i][j] = ++num;
+			}
+		}
+		for (Integer[] nums : randomGrid)
+			System.out.println(Arrays.toString(nums));
+		return randomGrid;
+	}
 
 }
