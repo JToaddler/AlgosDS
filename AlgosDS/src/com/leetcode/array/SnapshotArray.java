54ef157c308c269f9b0b111f0b7d06907183c3c2
JToaddler <JToaddler@users.noreply.github.com>
2023-06-11 15:40:15 -0400

    SnapShotArray updated

---
+++ AlgosDS/src/com/leetcode/array/SnapshotArray.java
@@ -0,0 +1,41 @@
+package com.leetcode.array;
+
+import java.util.TreeMap;
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray obj = new SnapshotArray(length); obj.set(index,val); int
+ * param_2 = obj.snap(); int param_3 = obj.get(index,snap_id);
+ */
+
+/**
+ * https://leetcode.com/problems/snapshot-array
+ * 
+ * @author Anto
+ *
+ */
+class SnapshotArray {
+
+	int snap = 0;
+	TreeMap<Integer, Integer>[] history;
+
+	public SnapshotArray(int length) {
+		history = new TreeMap[length];
+		for (int i = 0; i < length; i++) {
+			history[i] = new TreeMap<Integer, Integer>();
+			history[i].put(0, 0);
+		}
+	}
+
+	public void set(int index, int val) {
+		history[index].put(snap, val);
+	}
+
+	public int snap() {
+		return snap++;
+	}
+
+	public int get(int index, int snap_id) {
+		return history[index].floorEntry(snap_id).getValue();
+	}
+}
