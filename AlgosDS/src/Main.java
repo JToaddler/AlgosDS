4d3ed4d8ef6faaee2c79808a4a00212646c248f4
JToaddler <javasmoker@gmail.com>
2024-09-05 23:10:02 -0400

    test

--- AlgosDS/src/Main.java
+++ AlgosDS/src/Main.java
@@ -1,15 +1,40 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		int index = 2;
-		int i = 2;
-		index = i & 1;
-		System.out.println(index);
+		int[] nums = { 1, 1, 1, 2, 2, 3 };
+		topKFrequent(nums, 2);
+
+	}
+
+	public static int[] topKFrequent(int[] nums, int k) {
+		if (nums.length == 1)
+			return nums;
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i : nums) {
+			int count = map.getOrDefault(i, 0) + 1;
+			map.put(i, count);
+		}
+		System.out.println(map);
+		PriorityQueue<Integer> minHeap = new PriorityQueue<>((n1, n2) -> map.get(n1) - map.get(n1));
+
+		for (int i : nums) {
+			minHeap.add(i);
+			if (minHeap.size() > k)
+				minHeap.poll();
+		}
+		System.out.println(minHeap);
+		int[] arr = new int[k];
+		int i = 0;
+		while (i < k) {
+			arr[i] = minHeap.poll();
+			i++;
+		}
+		return arr;
 	}
 
 }
