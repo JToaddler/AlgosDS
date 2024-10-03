8cd222336c22bf65cc29bb40d7591bca72572d3a
JToaddler <6617138+JToaddler@users.noreply.github.com>
2024-10-02 20:51:21 -0400

    longest common prefix updated

--- leetcode-studyplan/src/main/java/leetcode/string/LongestCommonPrefix_14.java
+++ leetcode-studyplan/src/main/java/leetcode/string/LongestCommonPrefix_14.java
@@ -6,7 +6,8 @@ package leetcode.string;
 public class LongestCommonPrefix_14 {
 
 	public static void main(String[] args) {
-		
+		String[] strs = { "leetcode", "leets", "lee" };
+		System.out.println(longestCommonPrefix_verticalScan(strs));
 	}
 
 	public static String longestCommonPrefix_horizontalScan(String[] strs) {
@@ -21,6 +22,20 @@ public class LongestCommonPrefix_14 {
 			}
 		}
 		return prefix;
+	}
+
+	public static String longestCommonPrefix_verticalScan(String[] strs) {
+		if (strs == null || strs.length == 0)
+			return "";
+		for (int i = 0; i < strs[0].length(); i++) {
+			char c = strs[0].charAt(i);
+			for (int j = 1; j < strs.length; j++) {
+				if (i == strs[j].length() || strs[j].charAt(i) != c) {
+					return strs[0].substring(0, i);
+				}
+			}
+		}
+		return strs[0];
 	}
 
 }
