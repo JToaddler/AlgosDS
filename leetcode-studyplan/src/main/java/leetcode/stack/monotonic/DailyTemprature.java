9ab311ff9097e45a9ffd9f9064ccee783f850f94
JToaddler <6617138+JToaddler@users.noreply.github.com>
2024-10-22 16:11:13 -0400

    monotonic stack updated

--- leetcode-studyplan/src/main/java/leetcode/stack/monotonic/DailyTemprature.java
+++ leetcode-studyplan/src/main/java/leetcode/stack/monotonic/DailyTemprature.java
@@ -1,6 +1,10 @@
 package leetcode.stack.monotonic;
 
+import java.time.LocalDateTime;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.stream.IntStream;
 
 /**
  * https://leetcode.com/problems/daily-temperatures/
@@ -8,8 +12,17 @@ import java.util.Arrays;
 public class DailyTemprature {
 
 	public static void main(String[] args) {
-		int[] temp = { 30, 60, 90 };
+		int[] temp = new int[100000];
+		IntStream.range(0, 99999).forEach(a -> {
+			temp[a] = 99;
+		});
+		temp[99999] = 100;
+		System.out.println(Arrays.toString(temp));
+		System.out.println(LocalDateTime.now());
 		dailyTemperatures_BF(temp);
+		System.out.println(LocalDateTime.now());
+		dailyTemperatures_stack(temp);
+		System.out.println(LocalDateTime.now());
 	}
 
 	// time limit exceeded exception - TC - O(N^2)
@@ -19,7 +32,7 @@ public class DailyTemprature {
 		for (int i = 0; i < temperatures.length; i++) {
 			res[i] = findWarmerIndex(temperatures, i, temperatures[i]);
 		}
-		System.out.println(Arrays.toString(res));
+		//System.out.println(Arrays.toString(res));
 		return res;
 	}
 
@@ -29,6 +42,24 @@ public class DailyTemprature {
 				return j - i;
 		}
 		return 0;
+	}
+	
+	/* Optimized 
+	 * TC - O(2⋅N)= O(N)
+	 * SP - O(N) 
+	 */
+	public  static int[] dailyTemperatures_stack(int[] temperatures) {
+		int[] res = new int[temperatures.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int currDay = 0 ; currDay < temperatures.length; currDay++){
+            int currTemp = temperatures[currDay];
+            while(!stack.isEmpty() && temperatures[stack.peek()] < currTemp){
+                int prevDay = stack.pop();
+                res[prevDay] = currDay - prevDay;
+            }
+            stack.push(currDay);
+        }
+		return res;
 	}
 
 }
