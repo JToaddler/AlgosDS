56edae25040042426ffc50525fff82cd8c9ff049
JToaddler <6617138+JToaddler@users.noreply.github.com>
2024-11-03 15:23:37 -0500

    Notes TODO updated & linkedlist updated

---
+++ leetcode-studyplan/src/main/java/ds/linkedlist/ListNode.java
@@ -0,0 +1,15 @@
+package ds.linkedlist;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	public ListNode(int v) {
+		this.val = v;
+	}
+
+	public ListNode(int v, ListNode next) {
+		this.val = v;
+		this.next = next;
+	}
+}
